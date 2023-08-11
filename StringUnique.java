d5ab65b974b547e96b1fc3c56599c7a3b810b016
amit9669 <141909987+amit9669@users.noreply.github.com>
2023-08-11 16:17:04 +0530

    Add files via upload

---
+++ StringUnique.java
@@ -0,0 +1,36 @@
+package AssignmentSTRINGPDF;
+
+import java.util.Scanner;
+
+public class StringUnique {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner s = new Scanner (System.in) ; 
+		System.out.print("Enter the Size : ");
+		
+		String s1 [] = new String[s.nextInt()] ;
+		for (int i = 0; i < s1.length; i++) {
+			s1[i]=s.next();
+		}
+		for (int i = 0; i < s1.length; i++) {
+			int count = 0 ;
+			if(s1[i]!=null)
+			{
+				for (int j = i+1; j < s1.length; j++) {
+					if(s1[i].equals(s1[j]))
+					{
+						count++ ;
+						s1[j] = null ;
+					}
+				}
+				if(count ==0)
+				{
+				System.out.println(s1[i]); 
+				}
+			}
+		}
+	}
+
+}
